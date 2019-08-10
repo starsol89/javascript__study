b6ba3253873d1b688724a41fbeb1b4580afe81af
starsol89 <sinabro1143@gmail.com>
2019-08-10 10:30:09 +0900

    Making a To Do List part Three

--- Hello World with Javascript/todo.js
+++ Hello World with Javascript/todo.js
@@ -6,7 +6,24 @@ const toDoForm = document.querySelector(".js-toDoForm"),
 const TODOS_LS = 'toDos';
 
 //해야할 일을 생성할 때마다 'toDos'라는 array에 추가되도록 할것이다.
-const toDos = [];
+let toDos = [];
+
+function deleteToDo(event) {
+    const btn = event.target; // 우리가 누른 버튼에 대해서 html에 있는걸 선택해 준다. 예시) ❌를 누르면 거기에 해당하는 button을 선택되게 함.
+    const li = btn.parentNode; // btn에 담겨진 button의 부모가 누구인지 더 명확하게 알려준다. 여기선 li가 누군지.
+    toDoList.removeChild(li); // 명확하게 알게 된 button을 지워준다. 부모 자식 모두 지워짐.
+
+    //지금 사용 하려는 코딩은 toDos에 filter를 사용해 함수 filterFn를 동작하게 하는 것이다.
+    //filter를 이용하게 되면, array의 모든 아이템을 통해 함수를 실행하고 그리고 true(참)인 아이템들만 가지고 새로운 array를 만든다.
+    const cleanToDos = toDos.filter(function(toDo){
+        //모든 toDos가 'li'의 id와 같지 않을때 동작하며, parseInt는 li의 id를 string에서 int로 변환해준다. 왜냐면 지워줬더니 toDo.id는 숫자, li.id는 string이라 지워지지 않았음.
+        return toDo.id !== parseInt(li.id);
+    });
+    //cleanToDos에서 일어난 일련의 동작들의 결과를 toDos에 담고, saveToDos() 함수가 실행 됨.
+    toDos = cleanToDos;
+    //이 함수는 'toDos'를 저장 할 것이다.
+    saveToDos();
+}
 
 // 이 saveToDos 함수는 9번째 줄의 toDos를 가져와서 로컬스토리지에 저장하는 일을 하게 될 것이다.
 //JSON.stringify는 javascript object를 string으로 바꿔준다.
@@ -21,6 +38,9 @@ function paintToDo(text) {
     const span = document.createElement("span");//또 span를 만들었다.
     const newId = toDos.length + 1; //toDos.length + 1은 toDos에 기본으로 1을 지니고 있다.
     delBtn.innerText = "❌";
+    //paintToDo함수에 delBtn을 만들때 addEventListener 동작을 하게 끔 만든다. 그리고 addEventListener는 click 이벤트를 갖는다.
+    //클릭 하고 난 다음 동작하는 함수는 deleteToDo가 된다.
+    delBtn.addEventListener("click", deleteToDo);
     //이 text는 submit function 에서 온 값이다.
     span.innerText = text;
     //li.appendChild()를 해줄건데 그게 무엇이냐면, 뭔가를 그의 father element(부모요소)안에 넣는 것이다.
